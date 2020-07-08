52fc417213b080cd56f3df4b6b474394d88e4773
hemei <505349085@qq.com>
2020-07-08 14:42:52 +0800

    用例数据处理

---
+++ common/excel_utils.py
@@ -0,0 +1,89 @@
+import os
+import xlrd
+import json
+
+
+
+class Excel_utils():
+    def __init__(self,data_path,sheet_name):
+        self.data_path=data_path
+        self.sheet_name=sheet_name
+        self.sheet=self.open_sheet()
+
+    '''打开工作表'''
+
+    def open_sheet(self):
+        self.workbook=xlrd.open_workbook(self.data_path)
+        self.sheet=self.workbook.sheet_by_name(self.sheet_name)
+        return self.sheet
+
+    '''获取最大行数'''
+
+    def get_row_count(self):
+        nrows=self.sheet.nrows
+        return nrows
+
+    '''获取最大列数'''
+
+    def get_col_count(self):
+        cols=self.sheet.ncols
+        return cols
+
+    '''获取行值'''
+
+    def get_cell_value(self,row_index,col_index):
+        call_value=self.sheet.cell_value(row_index,col_index)
+        return call_value
+
+    '''获取合并单元格数量'''
+
+    def get_merged_index(self):
+        return self.sheet.merged_cells
+
+
+
+    """既能获取普通单元格的数据又能获取合并单元格数据"""
+
+    def get_merged_cell_value(self,row_index,col_index):
+        call_values = None
+        # 遍历合并单元格的起始行和结束行、起始列、结束列
+        for (rlow, rhigh, clow, chigh) in self.get_merged_index():
+            # 判断行数是否在合并单元格的行内
+            if (row_index >= rlow and row_index < rhigh):
+                # 判断列数是否在合并单元格的列内
+                if (col_index >= clow and col_index < chigh):
+                    call_values = self.sheet.cell_value(rlow, clow)
+                    break;
+                else:
+                    call_values = self.sheet.cell_value(row_index, col_index)
+            else:
+                call_values = self.sheet.cell_value(row_index, col_index)
+
+        return call_values
+
+    '''获取excel数据'''
+    def get_sheet_data_by_dict(self):
+        alldict = []
+        fisrt_nrows = self.sheet.row(0)
+        for row in range(1, self.get_row_count()):
+            dict = {}
+            for col in range(0, self.get_col_count()):
+                dict[fisrt_nrows[col].value] = self.get_merged_cell_value(row, col)
+            alldict.append(dict)
+        return alldict
+if __name__=="__main__":
+    current=os.path.dirname(__file__)
+    path_data_file=os.path.join(current,'../data/test_case.xlsx')
+    value=Excel_utils(path_data_file,'Sheet1').get_sheet_data_by_dict()
+    # print(json.dumps(value,indent=1,ensure_ascii=False))
+    all_case={}
+    castlist={}
+    for i in value:
+        all_case.setdefault(i['测试用例编号'],[]).append(i)
+
+
+    print(all_case)
+
+
+
+
---
+++ common/testdata_utils.py
@@ -0,0 +1,45 @@
+import os
+import xlrd
+import json
+from common.excel_utils import Excel_utils
+
+
+current = os.path.dirname(__file__)
+path_data_file = os.path.join(current, '../data/test_case.xlsx')
+
+
+class TestdataUtils():
+    def __init__(self,sheet_name,test_data_path=path_data_file):
+        self.test_data_path=test_data_path
+        self.test_data=Excel_utils(path_data_file, sheet_name).get_sheet_data_by_dict()
+
+    def get_testcase_data_dict(self):
+        all_case = {}
+        for i in self.test_data:
+            print(all_case.setdefault(i['测试用例编号'], []).append(i))
+        return all_case
+    def get_testcase_data_list(self):
+        data=self.get_testcase_data_dict()
+        list_data=[]
+        for key,value in data.items():
+            dict_data={}
+            dict_data['case_name']=key
+            dict_data['case_info']=value
+            list_data.append(dict_data)
+        return list_data
+
+
+
+
+if __name__=="__main__":
+    value =TestdataUtils('Sheet1',path_data_file).get_testcase_data_dict()
+    caselist=[]
+    for key,value in value.items():
+        dictlist = {}
+        dictlist['case_name']=key
+        dictlist['case_info']=value
+        caselist.append(dictlist)
+    value2=TestdataUtils('Sheet1',path_data_file).get_testcase_data_list()
+
+    print(json.dumps(value2, indent=1, ensure_ascii=False))
+
